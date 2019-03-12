83fb99d5d5ffbc80fc5f30ea90c8861a1edbc298
xiaoguige <44077532+xiaoguige@users.noreply.github.com>
2019-03-12 10:39:26 +0800

    Update Main.java
    add oneline hello xiaogui

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("hello xiaogui");
+        System.out.println("hello xiaogui");
     }
 }
